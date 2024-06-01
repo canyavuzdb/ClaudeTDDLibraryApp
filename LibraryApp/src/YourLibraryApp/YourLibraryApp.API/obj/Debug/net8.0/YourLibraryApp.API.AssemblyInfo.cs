61cdb9e434e7883e5c68f31060542bea200d42ef
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-06-02 00:50:32 +0300

    initial commit

--- LibraryApp/src/YourLibraryApp/YourLibraryApp.API/obj/Debug/net8.0/YourLibraryApp.API.AssemblyInfo.cs
+++ LibraryApp/src/YourLibraryApp/YourLibraryApp.API/obj/Debug/net8.0/YourLibraryApp.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YourLibraryApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccdef86ac7e426de680d7ccbc2fc1ff6efcacb90")]
 [assembly: System.Reflection.AssemblyProductAttribute("YourLibraryApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YourLibraryApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
