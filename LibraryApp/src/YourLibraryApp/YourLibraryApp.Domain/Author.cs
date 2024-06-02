8a76f8f0e41fca7eea16abdb18fdc63e4c769c89
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-06-02 22:31:01 +0300

    Author properties

---
+++ LibraryApp/src/YourLibraryApp/YourLibraryApp.Domain/Author.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YourLibraryApp.Domain
+{
+    // Author.cs
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Biography { get; set; }
+    }
+}
