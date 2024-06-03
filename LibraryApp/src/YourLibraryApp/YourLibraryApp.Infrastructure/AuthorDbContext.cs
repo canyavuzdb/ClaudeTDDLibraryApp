8c2101b69052f0bb01042bc5a74ac1a177390dbf
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-06-04 00:08:12 +0300

    dotnet build and run

--- LibraryApp/src/YourLibraryApp/YourLibraryApp.Infrastructure/AuthorDbContext.cs
+++ LibraryApp/src/YourLibraryApp/YourLibraryApp.Infrastructure/AuthorDbContext.cs
@@ -23,25 +23,4 @@ namespace YourLibraryApp.Infrastructure
         }
     }
 
-    public class AuthorConfiguration : IEntityTypeConfiguration<Author>
-    {
-        public void Configure(EntityTypeBuilder<Author> builder)
-        {
-            builder.ToTable("Authors"); // Veritabanındaki tabloyu belirtebilirsiniz
-
-            builder.HasKey(a => a.Id); // Primary key tanımlayabilirsiniz
-
-            builder.Property(a => a.Name)
-                .IsRequired()
-                .HasMaxLength(100);
-
-            builder.Property(a => a.Biography)
-                .HasMaxLength(500);
-
-            builder.HasMany(a => a.Books)
-                .WithOne(b => b.Author)
-                .HasForeignKey(b => b.AuthorId);
-        }
-    }
-
 }
