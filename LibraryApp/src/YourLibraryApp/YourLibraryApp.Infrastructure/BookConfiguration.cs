b4451b6f855be1b35b357b6105ff75665c602f0b
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-06-04 00:07:31 +0300

    Book Configuration

---
+++ LibraryApp/src/YourLibraryApp/YourLibraryApp.Infrastructure/BookConfiguration.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using YourLibraryApp.Domain;
+
+namespace YourLibraryApp.Infrastructure
+{
+    public class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.ToTable("Books");
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.Title)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(b => b.PublicationYear)
+                .IsRequired();
+
+            builder.Property(b => b.Genre)
+                .HasMaxLength(50);
+
+            builder.HasOne(b => b.Author)
+                .WithMany(a => a.Books)
+                .HasForeignKey(b => b.AuthorId);
+        }
+    }
+}
