1e3dc7153ffd3751278105f612f5591249fd207f
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-06-03 14:53:42 +0300

    IEntityTypeConfiguration

--- LibraryApp/src/YourLibraryApp/YourLibraryApp.Infrastructure/AuthorDbContext.cs
+++ LibraryApp/src/YourLibraryApp/YourLibraryApp.Infrastructure/AuthorDbContext.cs
@@ -4,18 +4,44 @@ using System.Linq;
 using System.Threading.Tasks;
 using YourLibraryApp.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace YourLibraryApp.Infrastructure
 {
-   public class AuthorDbContext : DbContext
-{
-    public DbSet<Author> Authors { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public class AuthorDbContext : DbContext
     {
-        // Veritabanı bağlantı stringinizi burada ayarlayın
-        optionsBuilder.UseSqlServer(@"Connection String;Database=YourLibraryDatabase");
+        public AuthorDbContext(DbContextOptions<AuthorDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AuthorConfiguration());
+        }
     }
-}
+
+    public class AuthorConfiguration : IEntityTypeConfiguration<Author>
+    {
+        public void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.ToTable("Authors"); // Veritabanındaki tabloyu belirtebilirsiniz
+
+            builder.HasKey(a => a.Id); // Primary key tanımlayabilirsiniz
+
+            builder.Property(a => a.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(a => a.Biography)
+                .HasMaxLength(500);
+
+            builder.HasMany(a => a.Books)
+                .WithOne(b => b.Author)
+                .HasForeignKey(b => b.AuthorId);
+        }
+    }
 
 }
