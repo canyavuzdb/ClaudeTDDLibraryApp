e9b2efbf5580b06e8dd2e36ce2ebcbcebe83aa19
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-06-04 00:06:15 +0300

    BooksController bug fix

--- LibraryApp/src/YourLibraryApp/YourLibraryApp.API/BooksController.cs
+++ LibraryApp/src/YourLibraryApp/YourLibraryApp.API/BooksController.cs
@@ -8,60 +8,60 @@ using YourLibraryApp.Domain;
 
 namespace YourLibraryApp.API
 {
-   // BooksController.cs
-[ApiController]
-[Route("api/[controller]")]
-public class BooksController : ControllerBase
-{
-    private readonly IBookService _bookService;
-
-    public BooksController(IBookService bookService)
+    // BooksController.cs
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
     {
-        _bookService = bookService;
-    }
+        private readonly IBookService _bookService;
 
-    [HttpGet]
-    public IEnumerable<Book> GetAllBooks()
-    {
-        return _bookService.GetAllBooks();
-    }
-
-    [HttpGet("{id}")]
-    public Book GetBookById(int id)
-    {
-        return _bookService.GetBookById(id);
-    }
-
-    [HttpPost]
-    public IActionResult AddBook(Book book)
-    {
-        _bookService.AddBook(book);
-        return CreatedAtAction(nameof(GetBookById), new { id = book.Id }, book);
-    }
-
-    [HttpPut("{id}")]
-    public IActionResult UpdateBook(int id, Book book)
-    {
-        if (id != book.Id)
+        public BooksController(IBookService bookService)
         {
-            return BadRequest();
+            _bookService = bookService;
         }
 
-        _bookService.UpdateBook(book);
-        return NoContent();
-    }
-
-    [HttpDelete("{id}")]
-    public IActionResult DeleteBook(int id)
-    {
-        var bookToDelete = _bookService.GetBookById(id);
-        if (bookToDelete == null)
+        [HttpGet]
+        public IEnumerable<Book> GetAllBooks()
         {
-            return NotFound();
+            return _bookService.GetAllBooks();
         }
 
-        _bookService.DeleteBook(id);
-        return NoContent();
+        [HttpGet("{id}")]
+        public Book GetBookById(int id)
+        {
+            return _bookService.GetBookById(id);
+        }
+
+        [HttpPost]
+        public IActionResult AddBook(Book book)
+        {
+            _bookService.AddBook(book);
+            return CreatedAtAction(nameof(GetBookById), new { id = book.Id }, book);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody] Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            _bookService.UpdateBook(book);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var bookToDelete = _bookService.GetBookById(id);
+            if (bookToDelete == null)
+            {
+                return NotFound();
+            }
+
+            _bookService.DeleteBook(id);
+            return NoContent();
+        }
     }
-}
 }
