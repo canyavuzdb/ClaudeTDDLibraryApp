fe48211b6094051758e059fa76609cde91da39c2
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-06-03 14:54:15 +0300

    Updated Author Repo

--- LibraryApp/src/YourLibraryApp/YourLibraryApp.Infrastructure/AuthorRepository.cs
+++ LibraryApp/src/YourLibraryApp/YourLibraryApp.Infrastructure/AuthorRepository.cs
@@ -10,45 +10,45 @@ using YourLibraryApp.Domain;
 namespace YourLibraryApp.Infrastructure
 {
     // YourLibraryApp.Infrastructure projesinde
-public class AuthorRepository : IAuthorRepository
-{
-    private readonly AuthorDbContext _dbContext;
-
-    public AuthorRepository(AuthorDbContext dbContext)
+    public class AuthorRepository : IAuthorRepository
     {
-        _dbContext = dbContext;
-    }
+        private readonly AuthorDbContext _dbContext;
 
-    public IEnumerable<Author> GetAllAuthors()
-    {
-        return _dbContext.Authors.ToList();
-    }
-
-    public Author GetAuthorById(int id)
-    {
-        return _dbContext.Authors.Find(id);
-    }
-
-    public void AddAuthor(Author author)
-    {
-        _dbContext.Authors.Add(author);
-        _dbContext.SaveChanges();
-    }
-
-    public void UpdateAuthor(Author author)
-    {
-        _dbContext.Entry(author).State = EntityState.Modified;
-        _dbContext.SaveChanges();
-    }
-
-    public void DeleteAuthor(int id)
-    {
-        var author = _dbContext.Authors.Find(id);
-        if (author != null)
+        public AuthorRepository(AuthorDbContext dbContext)
         {
-            _dbContext.Authors.Remove(author);
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Author> GetAllAuthors()
+        {
+            return _dbContext.Authors.ToList();
+        }
+
+        public Author GetAuthorById(int id)
+        {
+            return _dbContext.Authors.Find(id);
+        }
+
+        public void AddAuthor(Author author)
+        {
+            _dbContext.Authors.Add(author);
             _dbContext.SaveChanges();
         }
+
+        public void UpdateAuthor(Author author)
+        {
+            _dbContext.Authors.Update(author);
+            _dbContext.SaveChanges();
+        }
+
+        public void DeleteAuthor(int id)
+        {
+            var author = _dbContext.Authors.Find(id);
+            if (author != null)
+            {
+                _dbContext.Authors.Remove(author);
+                _dbContext.SaveChanges();
+            }
+        }
     }
-}
 }
