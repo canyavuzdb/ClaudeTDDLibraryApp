bcea7dd460d58a7d726519f5841bea7a5af21ac6
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-06-05 23:31:46 +0300

    editing general

--- LibraryApp/src/YourLibraryApp/YourLibraryApp.Domain/Author.cs
+++ LibraryApp/src/YourLibraryApp/YourLibraryApp.Domain/Author.cs
@@ -12,7 +12,7 @@ namespace YourLibraryApp.Domain
         public int Id { get; set; }
         public string Name { get; set; }
         public string Biography { get; set; }
-        public ICollection<Book> Books { get; set; } // Yeni eklenen özellik
+        public virtual ICollection<Book> Books { get; set; }               
     }
 
 }
