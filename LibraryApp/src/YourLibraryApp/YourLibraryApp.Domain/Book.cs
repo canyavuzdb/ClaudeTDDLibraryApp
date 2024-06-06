80c565bf40a3d27abc4ff69f7dba7a3c2c928fbc
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-06-06 23:37:55 +0300

    removden Author

--- LibraryApp/src/YourLibraryApp/YourLibraryApp.Domain/Book.cs
+++ LibraryApp/src/YourLibraryApp/YourLibraryApp.Domain/Book.cs
@@ -2,16 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace YourLibraryApp.Domain
+{public class Book
 {
-    public class Book
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public int AuthorId { get; set; }
-        public virtual Author Author { get; set; }
-        public int PublicationYear { get; set; }
-        public string Genre { get; set; }
-    }
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int AuthorId { get; set; }
+    public int PublicationYear { get; set; }
+    public string Genre { get; set; }
+}
 }
