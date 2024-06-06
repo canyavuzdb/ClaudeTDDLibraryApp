bfcd7b3cbdfab6a2960bdc22d0cde77734c7651f
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-06-06 18:03:20 +0300

    Modified

--- LibraryApp/src/YourLibraryApp/YourLibraryApp.Domain/Book.cs
+++ LibraryApp/src/YourLibraryApp/YourLibraryApp.Domain/Book.cs
@@ -5,13 +5,13 @@ using System.Threading.Tasks;
 
 namespace YourLibraryApp.Domain
 {
-   public class Book
-{
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public int AuthorId { get; set; }
-    public virtual Author Author { get; set; }
-    public int PublicationYear { get; set; }
-    public string Genre { get; set; }
-}
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public virtual Author Author { get; set; }
+        public int PublicationYear { get; set; }
+        public string Genre { get; set; }
+    }
 }
