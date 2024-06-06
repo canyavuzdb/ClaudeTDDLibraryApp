b7886166237b8435a91d1a0444ad3c0029197bda
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-06-06 18:04:53 +0300

    modified but...

--- LibraryApp/src/YourLibraryApp/YourLibraryApp.API/BooksController.cs
+++ LibraryApp/src/YourLibraryApp/YourLibraryApp.API/BooksController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using YourLibraryApp.Application;
 using YourLibraryApp.Domain;
 
@@ -33,11 +34,20 @@ namespace YourLibraryApp.API
         }
 
         [HttpPost]
-        public IActionResult AddBook(Book book)
+        public IActionResult AddBook([FromBody] string jsonString)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            // Newtonsoft.Json kütüphanesini kullanarak JSON'u Book nesnesine dönüştürün
+            var book = JsonConvert.DeserializeObject<Book>(jsonString);
+
+            // Verileri kontrol edin ve doğrulayın (gerekirse)
+
             _bookService.AddBook(book);
             return CreatedAtAction(nameof(GetBookById), new { id = book.Id }, book);
         }
+
 
         [HttpPut("{id}")]
         public IActionResult UpdateBook(int id, [FromBody] Book book)
