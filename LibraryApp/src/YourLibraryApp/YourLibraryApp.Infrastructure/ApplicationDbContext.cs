d1d8471838da0b6db10000c87848fce9672a1574
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-06-07 01:04:55 +0300

    ApplicationDbContext modified

--- LibraryApp/src/YourLibraryApp/YourLibraryApp.Infrastructure/ApplicationDbContext.cs
+++ LibraryApp/src/YourLibraryApp/YourLibraryApp.Infrastructure/ApplicationDbContext.cs
@@ -1,26 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using YourLibraryApp.Domain;
 
 namespace YourLibraryApp.Infrastructure
 {
-   public class ApplicationDbContext : DbContext
-{
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
+    public class ApplicationDbContext : DbContext
     {
-    }
+        public ApplicationDbContext() : base()
+        {
+        }
 
-    public DbSet<Author> Authors { get; set; }
-    public DbSet<Book> Books { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.ApplyConfiguration(new AuthorConfiguration());
-        modelBuilder.ApplyConfiguration(new BookConfiguration());
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AuthorConfiguration());
+            modelBuilder.ApplyConfiguration(new BookConfiguration());
+        }
     }
-}
 }
