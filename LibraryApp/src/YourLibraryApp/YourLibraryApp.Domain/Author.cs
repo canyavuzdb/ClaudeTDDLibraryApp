323f2fd0f531f8f933f63bf1df88bc5e29e8e4ba
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-06-08 00:38:10 +0300

    JsonIgnore added

--- LibraryApp/src/YourLibraryApp/YourLibraryApp.Domain/Author.cs
+++ LibraryApp/src/YourLibraryApp/YourLibraryApp.Domain/Author.cs
@@ -1,18 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace YourLibraryApp.Domain
 {
     // Author.cs
     public class Author
-
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Biography { get; set; }
-        public virtual ICollection<Book> Books { get; set; }               
+
+        [JsonIgnore]
+        public virtual ICollection<Book> Books { get; set; } = new List<Book>();
     }
 
 }
