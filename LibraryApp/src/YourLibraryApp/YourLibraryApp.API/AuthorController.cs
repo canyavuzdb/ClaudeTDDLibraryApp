218959658b6d24c2497f306ca4f60666c830fef1
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-06-08 00:37:47 +0300

    POST and PUT methods updated

--- LibraryApp/src/YourLibraryApp/YourLibraryApp.API/AuthorController.cs
+++ LibraryApp/src/YourLibraryApp/YourLibraryApp.API/AuthorController.cs
@@ -1,69 +1,62 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using YourLibraryApp.Application;
 using YourLibraryApp.Domain;
 
-
 namespace YourLibraryApp.API
 {
-    public class AuthorController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthorsController : ControllerBase
     {
-        [ApiController]
-        [Route("api/[controller]")]
-        public class AuthorsController : ControllerBase
+        private readonly IAuthorService _authorService;
+
+        public AuthorsController(IAuthorService authorService)
         {
-            private readonly IAuthorService _authorService;
+            _authorService = authorService;
+        }
 
-            public AuthorsController(IAuthorService authorService)
-            {
-                _authorService = authorService;
-            }
+        [HttpGet]
+        public IEnumerable<Author> GetAllAuthors()
+        {
+            return _authorService.GetAllAuthors();
+        }
 
-            [HttpGet]
-            public IEnumerable<Author> GetAllAuthors()
-            {
-                return _authorService.GetAllAuthors();
-            }
+        [HttpGet("{id}")]
+        public Author GetAuthorById(int id)
+        {
+            return _authorService.GetAuthorById(id);
+        }
 
-            [HttpGet("{id}")]
-            public Author GetAuthorById(int id)
-            {
-                return _authorService.GetAuthorById(id);
-            }
+        [HttpPost]
+        public IActionResult AddAuthor([FromBody] Author author)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
-            [HttpPost]
-            public IActionResult AddAuthor(Author author)
-            {
-                if (!ModelState.IsValid)
-                    return BadRequest(ModelState);
+            _authorService.AddAuthor(author);
+            return CreatedAtAction(nameof(GetAuthorById), new { id = author.Id }, author);
+        }
 
-                _authorService.AddAuthor(author);
-                return CreatedAtAction(nameof(GetAuthorById), new { id = author.Id }, author);
-            }
+        [HttpPut("{id}")]
+        public IActionResult UpdateAuthor(int id, [FromBody] Author author)
+        {
+            if (id != author.Id)
+                return BadRequest();
 
-            [HttpPut("{id}")]
-            public IActionResult UpdateAuthor(int id, Author author)
-            {
-                if (id != author.Id)
-                    return BadRequest();
+            _authorService.UpdateAuthor(author);
+            return NoContent();
+        }
 
-                _authorService.UpdateAuthor(author);
-                return NoContent();
-            }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteAuthor(int id)
+        {
+            var authorToDelete = _authorService.GetAuthorById(id);
+            if (authorToDelete == null)
+                return NotFound();
 
-            [HttpDelete("{id}")]
-            public IActionResult DeleteAuthor(int id)
-            {
-                var authorToDelete = _authorService.GetAuthorById(id);
-                if (authorToDelete == null)
-                    return NotFound();
-
-                _authorService.DeleteAuthor(id);
-                return NoContent();
-            }
+            _authorService.DeleteAuthor(id);
+            return NoContent();
         }
     }
 }
