401398a2e89794799e1dc4fe767be70f95f8efad
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-06-14 02:52:18 +0300

    Error correction required

--- LibraryApp/src/YourLibraryApp/YourLibraryApp.Application/obj/Debug/net8.0/YourLibraryApp.Application.AssemblyInfo.cs
+++ LibraryApp/src/YourLibraryApp/YourLibraryApp.Application/obj/Debug/net8.0/YourLibraryApp.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YourLibraryApp.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d10d8d90d1a442425b00c12dc3d3eb6bb01cc1ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a8e5427097f3cb93bb4b32954c8284941476c10")]
 [assembly: System.Reflection.AssemblyProductAttribute("YourLibraryApp.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YourLibraryApp.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
