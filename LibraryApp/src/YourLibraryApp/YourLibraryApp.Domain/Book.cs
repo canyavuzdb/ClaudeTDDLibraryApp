f1e2561ced8159488d0db3550c2df3c3b35432ef
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-06-14 02:50:36 +0300

    nullable

--- LibraryApp/src/YourLibraryApp/YourLibraryApp.Domain/Book.cs
+++ LibraryApp/src/YourLibraryApp/YourLibraryApp.Domain/Book.cs
@@ -10,12 +10,12 @@ namespace YourLibraryApp.Domain
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public int AuthorId { get; set; }
+        public int? AuthorId { get; set; }
 
         [JsonIgnore]
         public virtual Author Author { get; set; }
 
-        public int PublicationYear { get; set; }
+        public int? PublicationYear { get; set; }
         public string Genre { get; set; }
     }
 }
