36d7fb4ac1fe445cbae1c36aa212144e188be503
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-06-14 14:52:09 +0300

    Migrations AllowNullBiography

---
+++ LibraryApp/src/YourLibraryApp/YourLibraryApp.Infrastructure/Migrations/20240614113653_AllowNullBiography.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YourLibraryApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AllowNullBiography : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Author_AuthorId",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AuthorId",
+                table: "Books",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Biography",
+                table: "Author",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Author_AuthorId",
+                table: "Books",
+                column: "AuthorId",
+                principalTable: "Author",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Author_AuthorId",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AuthorId",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Biography",
+                table: "Author",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000,
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Author_AuthorId",
+                table: "Books",
+                column: "AuthorId",
+                principalTable: "Author",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
