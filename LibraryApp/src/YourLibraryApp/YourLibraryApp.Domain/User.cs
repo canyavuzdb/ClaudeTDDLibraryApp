e9ed71f941ff841c4da6bd45366e499e41a6716d
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-07-16 23:06:17 +0300

    User entity

---
+++ LibraryApp/src/YourLibraryApp/YourLibraryApp.Domain/User.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YourLibraryApp.Domain
+{
+    public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public string Role { get; set; } // "Admin" veya "User"
+}
+}
