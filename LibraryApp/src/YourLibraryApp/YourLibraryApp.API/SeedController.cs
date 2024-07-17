0ae2a4280342eb491f40902cc0dc9780f74df570
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-07-18 01:35:28 +0300

    Update SeedController POST method added into

--- LibraryApp/src/YourLibraryApp/YourLibraryApp.API/SeedController.cs
+++ LibraryApp/src/YourLibraryApp/YourLibraryApp.API/SeedController.cs
@@ -12,10 +12,12 @@ namespace YourLibraryApp.API
     public class SeedController : ControllerBase
     {
         private readonly IOpenLibraryService _openLibraryService;
+        private readonly IBookService _bookService;
 
-        public SeedController(IOpenLibraryService openLibraryService)
+        public SeedController(IOpenLibraryService openLibraryService, IBookService bookService)
         {
             _openLibraryService = openLibraryService;
+            _bookService = bookService;
         }
 
         [HttpGet("search")]
@@ -23,6 +25,14 @@ namespace YourLibraryApp.API
         {
             var books = await _openLibraryService.SearchBooksAsync(query, maxResults);
             return Ok(books);
+        }
+
+        [HttpPost("seed")]
+        public async Task<IActionResult> SeedBooks(string query, int maxResults = 40)
+        {
+            var books = await _openLibraryService.SearchBooksAsync(query, maxResults);
+            var addedBooks = await _bookService.AddBooksAsync(books);
+            return Ok($"{addedBooks} books have been added to the database.");
         }
     }
 }
